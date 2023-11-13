0bee80d24a652a4a41f20eb509ca990ba4e6390e
maximus <maximusagub@gmail.com>
2023-11-13 15:36:51 -0600

    added fourm validation on page index.html - Maximus

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // Get a reference to the button element
-const submitButton = document.getElementById("submitButton");
+const submitButtonIndex = document.getElementById("submitButton_indx");
+const first_name_input_index = document.getElementById("first_name_input_index")
+const last_name_input_index = document.getElementById("last_name_input_index")
+const phone_number_input_index = document.getElementById("phone_number_input_index")
+
+function isValid() {
+  if (first_name_input_index.value.trim() === "" || last_name_input_index.value.trim() === "" || phone_number_input_index.value.trim() === "" ) {
+    return alert("hmm, ensure to anser all questions before moving on.");
+  } else {
+    return true;
+  }
+}
 
 // Add a click event listener to the button
-submitButton.addEventListener("click", function() {
+submitButtonIndex.addEventListener("click", function() {
   // Redirect to a different HTML page when the button is clicked
-  console.log("Hit");
-  window.location.href = "Team-Project-WDM/pages/second_page.html";
+  isFormValid = isValid();
+  if (isFormValid) { 
+    window.location.href = "second_page.html";
+  } else {
+    // soft handel via alert.
+  }
 });
